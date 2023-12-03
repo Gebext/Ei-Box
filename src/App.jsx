bb450d7c0d770d20badf8ba389e2c047c0b94472
Gebext <gibrand987@gmail.com>
2023-12-03 21:10:06 +0700

    First Setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        {/* WAHYU IMPORT 👇 */}
+
+        {/* GIBRAN IMPORT 👇 */}
+        <Route path="/" />
+        {/* AFRA IMPORT 👇 */}
+
+        {/* KHALID IMPORT 👇 */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
